b337d6148d263aa1cb93c60353b2cbc2f8964dd7
fyfitzgerald <fyfitzgerald@gmail.com>
2018-09-28 21:29:25 -0700

    Removed folder session_words from repository

--- session_words/apps/first_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views           # This line is new!
-
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^add$', views.add),
-    url(r'^clear$', views.clear),   # This line has changed! Notice that urlpatterns is a list, the comma is in
-]                           
